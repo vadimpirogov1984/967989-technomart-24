93bc848034dbe611566755b1a3cbdc126e57231e
Vadim <vadim_pirogov@mail.ru>
2020-02-28 09:40:17 +0300

    Упорядочил код

--- js/main.js
+++ js/main.js
@@ -1,17 +1,85 @@
-$(function() {
-  $("#slider-range").slider({
-    range: true,
-    min: 0,
-    max: 40000,
-    values: [0, 30000],
-    slide: function(event, ui) {
-      $("#amount").val(ui.values[0]);
-      $("#amount_1").val(ui.values[1]);
-    }
+$(document).ready(function() {
+  $(function() {
+    $(".multiple-review").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      prevArrow:
+        '<img src="../img/icon-left.png" class="slick-arrow slick-prev">',
+      nextArrow:
+        '<img src="../img/icon-right.png" class="slick-arrow slick-next">'
+    });
   });
-  $("#amount").val($("#slider-range").slider("values", 0));
-  $("#amount_1").val($("#slider-range").slider("values", 1));
+
+  $(function() {
+    $("#slider-range").slider({
+      range: true,
+      min: 0,
+      max: 40000,
+      values: [0, 30000],
+      slide: function(event, ui) {
+        $("#amount").val(ui.values[0]);
+        $("#amount_1").val(ui.values[1]);
+      }
+    });
+    $("#amount").val($("#slider-range").slider("values", 0));
+    $("#amount_1").val($("#slider-range").slider("values", 1));
+  });
 });
+
+// Всплывающее окно "Войти"
+
+let link = document.querySelector(".login-link");
+let popup = document.querySelector(".modal-login");
+let close = popup.querySelector(".login-form-img");
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+// // Всплывающее окно карты
+// let write = document.querySelector(".mybtn");
+// let popupWrite = document.querySelector(".overlay");
+// let closeWrite = popupWrite.querySelector(".close-popup");
+
+// write.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   popupWrite.classList.add("modal-show");
+// });
+
+// closeWrite.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   popupWrite.classList.remove("modal-show");
+// });
+
+// // Всплывающая форма
+// let links = document.querySelector(".write-link");
+// let popups = document.querySelector(".modal__write");
+// let closes = popup.querySelector(".login-form-img");
+// // let login = popup.querySelector("[name=login]");
+
+// links.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   popups.classList.add("modal-show");
+//   // login.focus();
+// });
+
+// closes.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   popups.classList.remove("modal-show");
+// });
+
+// Сортировка по возрастанию/ убыванию
 
 document.querySelector("#price").onclick = function() {
   document.querySelector("#sort-up").onclick = function() {
@@ -48,7 +116,6 @@ function sortList(sortType) {
         +items.children[i].getAttribute(sortType) >
         +items.children[j].getAttribute(sortType)
       ) {
-        // console.log(1);
         let replacedNode = items.replaceChild(
           items.children[j],
           items.children[i]
@@ -67,7 +134,6 @@ function sortListDesc(sortType) {
         +items.children[i].getAttribute(sortType) <
         +items.children[j].getAttribute(sortType)
       ) {
-        // console.log(1);
         let replacedNode = items.replaceChild(
           items.children[j],
           items.children[i]
@@ -81,19 +147,3 @@ function sortListDesc(sortType) {
 function insertAfter(elem, refElem) {
   return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
 }
-
-let link = document.querySelector(".login-link");
-let popup = document.querySelector(".modal-login");
-var close = popup.querySelector(".login-form-img");
-// var login = popup.querySelector("[name=login]");
-
-link.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-  login.focus();
-});
-
-close.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-});
